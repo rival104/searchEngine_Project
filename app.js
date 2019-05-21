844757482e10506709353a8fc5b7bde65ff23dc0
rival104 <navid.rvl@gmail.com>
2019-05-21 18:52:28 +0000

    Finished phase 5 v1

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const extractor = require('unfluff');
+const bodyParser  = require("body-parser");
 const request = require('request');
 const rp = require('request-promise');
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+var ObjectId = require('mongodb').ObjectId;
 
+//connect to database
 mongoose.connect('mongodb+srv://dbUser:dbGt1213@cluster0-9onmf.mongodb.net/test?retryWrites=true', {
 	useNewUrlParser: true,
 	useCreateIndex: true
@@ -21,8 +26,9 @@ const PWord= require('./pageWordSchema.js');
 const Search= require('./searchSchema.js');
 
 
-
-function crawlerEngine(urls){
+//Indexing Engine
+function crawlerEngine(urls, rec){
+	
 	rp(urls)
     .then(function (htmlString) {
         // Process html...
@@ -38,6 +44,8 @@ function crawlerEngine(urls){
 		console.log(data.date);
 		console.log(data.canonicalLink);
 		console.log(date);
+		
+		
 		// console.log(data.text);
 		
 		//readline
@@ -63,7 +71,8 @@ function crawlerEngine(urls){
 		// for (const [word, freq] of entries) {
 		//   console.log(`There are ${freq} ${word}s`);
 		// }
-
+		
+		
 		//databse entry 
 		Page.create({
 			url:			data.canonicalLink,
@@ -75,35 +84,98 @@ function crawlerEngine(urls){
 		},function(err, pResult){
 			if(err) console.log(err);
 			else{
-				for (const [word, freq] of entries) {
-				  console.log(`There are ${freq} ${word}s`);
+				for (let [word, freq] of entries) {
+					word = word.toLowerCase();
+				  // console.log(`There are ${freq} ${word}s`);
 					
-						Word.create({
-						wordName:	word
-					}, function (err2, wResult){
-						if(err2) console.log(err2);
+					//if word find empty
+					Word.find({wordName:word}, function(errf, wResult){
+						if(errf) console.log(errf);
 						else{
-							PWord.create({
-								pageId: [ 
-									pResult._id 
-								],
-								wordId: [
-									wResult._id 
-								],
-								frequency: freq
-							}, function(err3, pWResult){
-								if(err3) console.log(err3);
+							if(wResult.length == 0){
+								Word.create({wordName:	word}, function (err2, wResult2){
+								if(err2) console.log(err2);
 								else{
-									console.log("CREATED!!!!");
+									// console.log("in create");
+									// console.log(wResult2);
+									PWord.create({
+										pageId: [ 
+											pResult._id 
+										],
+										wordId: [
+											wResult2._id 
+										],
+										frequency: freq
+									}, function(err3, pWResult){
+										if(err3) console.log(err3);
+										else{
+											console.log("CREATED!!!!");
+										}
+									});
 								}
-							});
+								});
+							}else{
+								// console.log("in found");
+									// console.log(wResult);
+								PWord.create({
+										pageId: [ 
+											pResult._id 
+										],
+										wordId: [
+											wResult[Object.keys(wResult)[0]]._id 
+										],
+										frequency: freq
+									}, function(err3, pWResult){
+										if(err3) console.log(err3);
+										else{
+											console.log("Updated!!!!");
+										}
+									});
+								// console.log("found; " + word);
+								// Word.find({wordName:word}, function (err3, wResult){
+								// if(err3) console.log(err3);
+								// else{
+								// 	console.log("in found");
+								// 	console.log(wResult);
+								// 	PWord.create({
+								// 		pageId: [ 
+								// 			pResult._id 
+								// 		],
+								// 		wordId: [
+								// 			wResult._id 
+								// 		],
+								// 		frequency: freq
+								// 	}, function(err3, pWResult){
+								// 		if(err3) console.log(err3);
+								// 		else{
+								// 			console.log("Updated!!!!");
+								// 		}
+								// 	});
+								// }
+								// });
+							}
+							
 						}
 					});
+						
 				}
 				
 				
 			}
-		});
+		});//page create
+		
+		//recursive search
+		if(rec == "on"){
+			console.log(data.links);
+			const href = Object.entries(data.links);
+			for (let [prop, obj] of href) {
+				if(obj.href.length > 10){
+					console.log(obj.href);
+					crawlerEngine(obj.href);
+				}	
+			}
+		}
+		
 
     })
     .catch(function (err) {
@@ -118,18 +190,154 @@ function crawlerEngine(urls){
 
 
 
+//Routing
 
 app.get('/', function (req, res){
 	// res.send("Hello World!");
 	// let post = await Word.create({wordName: 'Janfog'});
+	// console.log(Page.find({title: "Shovel Knight review"}));
+	// Page.find({title: "Shovel Knight review"}, function(err, page){
+	// if(err){
+	// console.log("OH NO, ERROR!");
+	// console.log(err);
+	// } else {
+	// console.log("ALL THE CATS.....");
+	// console.log(page);
+	// }
+	// });
 	
-	res.render("homepage.ejs");
+	
+	// Word.find({wordName: "past"}, function(err, word){
+	// 	if(err){
+	// 		console.log("OH NO, ERROR!");
+	// 		console.log(err);
+	// 	} else {
+	// 		console.log("ALL THE WORDS.....");
+	// 		console.log(word);
+	// 		console.log(word[Object.keys(word)[0]]._id);
+	// 		console.log(word.length == 0);
+
+	// 	}
+	// 	});
+	
+	// PWord.find({wordId: ObjectId("5ce36588c8eef57697a6f416")}, function(err, pw){
+	// 		if(err){
+	// 			console.log("OH NO, ERROR!");
+	// 			console.log(err);
+	// 		} else {
+	// 			console.log("ALL THE pw.....");
+	// 			console.log(pw);
+	// 		}
+	// });
+	
+	// var o_id = new ObjectId("5ce3f8891347db09e19f42c7");
+	// PWord.find({pageId: o_id}, function(err, pw){
+	// 		if(err){
+	// 			console.log("OH NO, ERROR!");
+	// 			console.log(err);
+	// 		} else {
+	// 			console.log("ALL THE pw.....");
+	// 			console.log(pw);
+	// 		}
+	// 		});
+	
+	res.render("homepage");
+	console.log("done");
 });
 
-app.get('/speak/:animal', function(req, res){
-	var animal = req.params.animal;
-	res.send("The "+animal+" says " + "Woof!");
+app.get("/addIndex", function(req, res){
+    var newIndex = req.query.newUrl;
+	var rec = req.query.rec;
+	console.log("rec");
+	console.log(rec);
+    crawlerEngine(newIndex, rec);
+	// res.send("<script>\r\nfunction myFunction() {\r\n  alert(\"Url information added to database\");\r\n}\r\nmyFunction()\r\n</script>");
+    res.redirect("/");
 });
+
+app.get("/showResults", async function(req, res){
+    var term = req.query.searchTerm;
+	if(req.query.caseIgnore == "on"){
+		term = term.toLowerCase();
+	}
+	   Word.find({wordName: term}, function(err, word){
+		if(err || word.length == 0){
+			console.log("OH NO, ERROR!");
+			console.log(err);
+			res.send("Not Found");
+		} else {
+			console.log("ALL THE WORDS.....");
+			console.log(word);
+			var wordID = word[Object.keys(word)[0]]._id;
+			var o_id = new ObjectId(wordID);
+			
+			PWord.find({wordId: o_id}, function(err, pw){
+			if(err){
+				console.log("OH NO, ERROR!");
+				console.log(err);
+			} else {
+				console.log("ALL THE pw.....");
+				console.log(pw[Object.keys(word)[0]].pageId);
+				var id = pw[Object.keys(word)[0]].pageId;
+				
+				//search History
+				const date = new Date();
+				var count = 0;
+				for (var k in pw) {
+					if (pw.hasOwnProperty(k)) {
+					   ++count;
+					}
+				}
+				Search.create({
+					terms:			term,
+					count: 			count,
+					searchDate:		date,
+					timeToSearch:	500
+				},function(err, pResult){
+				});
+				
+				// Page.findById(id, 'title desc', function (err, page) {console.log(page);});
+				
+				// for (const prop in pw) {
+				// 	 Page.findById(pw[prop].pageId, 'title desc url', function (err,page){  // console.log(page);
+				// 	 });
+				// }
+				
+			
+				res.render("results", {data: pw});
+			}
+			});
+		}
+		});
+    // res.redirect("/");
+});
+
+app.get("/page/:id", function(req, res){
+    //find the page with provided ID
+	Page.findById(req.params.id, function (err, page){ 
+		if(err){
+            console.log(err);
+        } else {     
+            //render show template with that page
+            res.render("page", {page: page});
+        }     
+    });
+});
+
+app.get("/searchH", function(req, res){
+    //find the page with provided ID
+	Search.find(function (err, sh){ 
+		if(err){
+            console.log(err);
+        } else {     
+            //render show template with that page
+			
+            res.render("searchH", {data: sh});
+        }     
+    });
+});
+
+
 
 app.listen(3000, function(){
 	console.log("Server has started!");
